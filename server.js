b4a4629ad08f6fcece5fdcc80f80f07c79e62a2b
Behnam Hamidi <eslp91@hotmail.com>
2024-04-25 00:27:34 +0330

    start api

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
+const mongoose = require("mongoose");
+const env = require("dotenv").config();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (env.error) {
+  throw env.error;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello from server!");
+});
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI);
+    console.log("Connected to the database!");
+  } catch (error) {
+    console.error("Connection failed!", error);
+  }
+};
+
+connect();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
